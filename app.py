1be7570dc9a90819bb8e8a2f055bb94b85575674
Stefano <stefano@start2impact.com>
2024-06-25 22:06:57 +0200

    Aggiunto istogramma

--- app.py
+++ app.py
@@ -19,9 +19,11 @@ def load_data(nrows):
 
 st.title('Uber pickups in NYC')
 
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
 data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text('Loading data...done!')
+
+st.subheader('Mostro dati grezzi')
+st.write(data)
+
+st.subheader('Numero di pickup per ora della giornata')
+hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.bar_chart(hist_values)
