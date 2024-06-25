dcbfd08d95ec0e7146321fe0175802fbccd53505
Stefano <stefano@start2impact.com>
2024-06-25 22:31:10 +0200

    Aggiunta mappa pickup

--- app.py
+++ app.py
@@ -22,8 +22,15 @@ st.title('Uber pickups in NYC')
 data = load_data(10000)
 
 st.subheader('Mostro dati grezzi')
-st.write(data)
+if st.checkbox('mostra'):
+    st.write(data)
+
 
 st.subheader('Numero di pickup per ora della giornata')
 hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
+
+hour_to_filter = st.slider('hour', 0, 23, 17)
+filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
+st.subheader('Mappa dei pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
